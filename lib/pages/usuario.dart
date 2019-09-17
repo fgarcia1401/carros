256ef4fada90bb771cdd02273b5767c052e311f8
Fernando Garcia <fgarcia@gmail.com>
2019-09-17 17:45:14 -0300

    add library http e parser lista

--- lib/pages/usuario.dart
+++ lib/pages/usuario.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 class Usuario {
   String login;
   String nome;
@@ -10,11 +12,17 @@ class Usuario {
      login = map["login"],
      token = map["token"],
      nome  = map["nome"],
-     email = map["email"];
+     email = map["email"],
+     roles = getRoles(map);
 
   @override
   String toString() {
     return 'Usuario{login: $login, nome: $nome, email: $email, token: $token, roles: $roles}';
+  }
+
+  static List<String> getRoles(Map<String, dynamic> map) {
+    List list = map["roles"];
+    return list.map<String>((role) => role.toString()).toList();
   }
 
 
