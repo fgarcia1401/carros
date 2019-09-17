3a89277e0e90ac26e28e572935b1ee4d67889170
Fernando Garcia <fgarcia@gmail.com>
2019-09-17 18:16:26 -0300

    library json serializable

--- lib/pages/usuario.dart
+++ lib/pages/usuario.dart
@@ -1,22 +1,25 @@
 
+import 'package:json_annotation/json_annotation.dart';
+
+part 'usuario.g.dart';
+
+@JsonSerializable(nullable: false)
 class Usuario {
   String login;
   String nome;
   String email;
   String token;
 
-  List<String> roles;
+  Usuario(this.login, this.nome, this.email, this.token);
 
-  Usuario.fromJson(Map<String, dynamic> map) :
-     login = map["login"],
-     token = map["token"],
-     nome  = map["nome"],
-     email = map["email"],
-     roles = map["roles"] != null ? map["roles"].map<String>((role) => role.toString()).toList() : "";
+  factory Usuario.fromJson(Map<String, dynamic> json) =>
+      _$UsuarioFromJson(json);
+
+  Map<String,dynamic> toJson() => _$UsuarioToJson(this);
 
   @override
   String toString() {
-    return 'Usuario{login: $login, nome: $nome, email: $email, token: $token, roles: $roles}';
+    return 'Usuario{login: $login, nome: $nome, email: $email, token: $token}';
   }
 
 
